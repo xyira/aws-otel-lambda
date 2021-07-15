70a4f45bdbaa5ee01e8fff5a2cd818ae14c0d644
Karen Xu <karenyrxu@gmail.com>
2021-07-15 18:01:25 -0400

    wait 10s before lambda shuts down

--- sample-apps/java-metrics-prometheus/SampleMetricsApp/src/main/java/metrics/App.java
+++ sample-apps/java-metrics-prometheus/SampleMetricsApp/src/main/java/metrics/App.java
@@ -21,8 +21,13 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         metricEmitter.emitQueueSizeChangeMetric(1, "/lambda-sample-app", "200");
         System.out.println("[I!]Returning from lambda handler...");
 
-        // TODO: Call forceFlush() once Java Metrics SDK includes it
-
+        // TODO: Call forceFlush() once Java Metrics SDK includes it, 
+        // Temporary hack: 
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return response.withStatusCode(200).withBody("Status Code 200");
     }
 }
