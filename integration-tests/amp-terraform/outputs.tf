9c74e04a343f6ec77b8d83144695a545fe65c37a
Kelvin Lo <kello@live.ca>
2021-08-12 16:21:37 -0700

    include collector config into outputs

--- integration-tests/amp-terraform/outputs.tf
+++ integration-tests/amp-terraform/outputs.tf
@@ -5,3 +5,36 @@ output "amp_endpoint" {
 output "remotewrite_endpoint" {
   value = "${aws_prometheus_workspace.lambda-test.prometheus_endpoint}api/v1/remote_write"
 }
+
+output "collector_config" {
+  value = yamlencode({
+    "receivers" : {
+      "otlp": {
+        "protocols": {
+          "grpc": ""
+          "http": ""
+        }
+      }
+    },
+    "exporters" : {
+      "logging": ""
+      "awsxray": ""
+      "awsprometheusremotewrite": {
+        "endpoint": "${aws_prometheus_workspace.lambda-test.prometheus_endpoint}api/v1/remote_write"
+      }
+    },
+    "service": {
+      "pipelines": {
+        "traces": {
+          "receivers": ["otlp"]
+          "exporters": ["awsxray"]
+        },
+        "metrics": {
+          "receivers": ["otlp"]
+          "exporters": ["logging", "awsprometheusremotewrite"] 
+        }
+      }
+    }
+  })
+}
+
